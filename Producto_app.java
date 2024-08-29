081b14fabae6b2795fcdb81f2f22a5b8408a195c
CarlosM <carlos4.martinez@ucp.edu.co>
2024-08-29 11:00:00 -0500

    Update Producto_app.java

---
+++ Producto_app.java
@@ -0,0 +1,37 @@
+package PRODUCTO_APP;
+//Librerias
+import javax.swing.*;
+
+
+
+//Funcion Main 
+public class Producto_app {
+
+    public static void main(String[] args) {
+        
+}
+// Metodo para crear el Formulario principal
+private void Interfaz() {
+    //Creamos el Formulario Principal
+    JFrame formulario = new JFrame("Gestion de Productos - Droguería la Principal - Pereira");
+    //
+}
+}
+
+dwdwdwdwdwdwdwdwdwdwdwdwdwdwdwdwdwdwdwdwdwd
+//Variables 
+
+
+
+
+
+//Diseño de pestañas
+
+
+
+
+
+//Creacion de pestañas y funciones 
+
+
+
